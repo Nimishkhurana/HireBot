2bf314f5001ad3778d5e5af56f8e527acab005f0
Nimish Khurana <nimishkhurana9@gmail.com>
2019-09-16 17:52:00 +0530

    Update audio_analysis.py

--- interviewer app/audio_analysis.py
+++ interviewer app/audio_analysis.py
@@ -57,26 +57,26 @@ for dict_ in speech_recognition_results['results']:
 with open('ans1.txt','w') as f:
     f.write(text)
     
-# import json
-# from ibm_watson import ToneAnalyzerV3
+import json
+from ibm_watson import ToneAnalyzerV3
 
 
-# tone_analyzer = ToneAnalyzerV3(
-#     version='2017-09-21',
-#     iam_apikey='s5dlNRmCgOWKmStaFyhn17dehz0kCpqlk7UfRViT0TS9',
-#     url='https://gateway-lon.watsonplatform.net/tone-analyzer/api'
-# )
+tone_analyzer = ToneAnalyzerV3(
+    version='2017-09-21',
+    iam_apikey='s5dlNRmCgOWKmStaFyhn17dehz0kCpqlk7UfRViT0TS9',
+    url='https://gateway-lon.watsonplatform.net/tone-analyzer/api'
+)
 
 
-# text = text
+text = text
 
 
 
-# tone_analysis1 = tone_analyzer.tone(
-#     {'text': text},
-#     content_type='application/json'
-# ).get_result()
-# # print(json.dumps(tone_analysis1, indent=2))
+tone_analysis1 = tone_analyzer.tone(
+    {'text': text},
+    content_type='application/json'
+).get_result()
+# print(json.dumps(tone_analysis1, indent=2))
     
     
     
@@ -84,49 +84,49 @@ with open('ans1.txt','w') as f:
     
     
 
-# document_tones=[]
-# for dicts in tone_analysis1['document_tone']['tones']:
-#     document_tones.append((dicts['tone_name'],dicts['score']))
+document_tones=[]
+for dicts in tone_analysis1['document_tone']['tones']:
+    document_tones.append((dicts['tone_name'],dicts['score']))
     
     
-# sentences_tone_pairs=[]
-# for total_sentences in tone_analysis1['sentences_tone']:
-#     a1=total_sentences['tones']
-#     for tones in a1:
-#         sentences_tone_pairs.append((tones['tone_name'],tones['score']))
+sentences_tone_pairs=[]
+for total_sentences in tone_analysis1['sentences_tone']:
+    a1=total_sentences['tones']
+    for tones in a1:
+        sentences_tone_pairs.append((tones['tone_name'],tones['score']))
 
-# Analytical=[]
-# Tentaive=[]
-# Sadness=[]
-# Joy=[]
-# Confident=[]
-# for pair in sentences_tone_pairs:
-#     if pair[0]=="Analytical":
-#         Analytical.append(pair)
-#     elif pair[0]=="Tentaive":
-#         Tentaive.append(pair)
-#     elif pair[0]=="Sadness":
-#         Sadness.append(pair)
-#     elif pair[0]=="Joy":
-#         Joy.append(pair)
-#     elif pair[0]=="Confident":
-#         Confidence.append(pair)
+Analytical=[]
+Tentaive=[]
+Sadness=[]
+Joy=[]
+Confident=[]
+for pair in sentences_tone_pairs:
+    if pair[0]=="Analytical":
+        Analytical.append(pair)
+    elif pair[0]=="Tentaive":
+        Tentaive.append(pair)
+    elif pair[0]=="Sadness":
+        Sadness.append(pair)
+    elif pair[0]=="Joy":
+        Joy.append(pair)
+    elif pair[0]=="Confident":
+        Confidence.append(pair)
         
-# Analytical_count=len(Analytical)
-# Tentaive_count=len(Tentaive)
-# Sadness_count=len(Sadness)
-# Joy_count=len(Joy)
-# Confident_count=len(Confident)
+Analytical_count=len(Analytical)
+Tentaive_count=len(Tentaive)
+Sadness_count=len(Sadness)
+Joy_count=len(Joy)
+Confident_count=len(Confident)
 
-# def total_score(trait):
-#     c=0
-#     for pair in trait:
-#         s=pair[1]+c
-#         c=s
-#     return(c) 
+def total_score(trait):
+    c=0
+    for pair in trait:
+        s=pair[1]+c
+        c=s
+    return(c) 
 
-# Total_Analytical_Score=total_score(Analytical)
-# Total_Tentaive_Score=total_score(Tentaive)
-# Total_Sadness_Score=total_score(Sadness)
-# Total_Joy_Score=total_score(Joy)
-# Total_Confident_Score=total_score(Confident)
+Total_Analytical_Score=total_score(Analytical)
+Total_Tentaive_Score=total_score(Tentaive)
+Total_Sadness_Score=total_score(Sadness)
+Total_Joy_Score=total_score(Joy)
+Total_Confident_Score=total_score(Confident)
